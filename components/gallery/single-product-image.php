36aad2cc49f6e6acad47677fb453fc39ea94ef7b
Vladimir Samojlov <v-moi@ya.ru>
2021-04-21 23:09:55 +0300

    edit

--- components/gallery/single-product-image.php
+++ components/gallery/single-product-image.php
@@ -15,7 +15,7 @@ $wrapper_classes   = apply_filters( 'woocommerce_single_product_image_gallery_cl
 	<?php 
 	global $product;
 	$attachment_ids = $product->get_gallery_image_ids();
-	if ( $attachment_ids && $product->get_image_id() ): ?>
+	if ( $attachment_ids && $product->get_image_id() || $product->get_image() ): ?>
 	<div class="product_gallery flex flex-col-reverse md:flex-row">
 		<div class="product_gallery_list flex flex-row md:flex-col p-2 md:p-5">
 			<!-- Показываем остальные -->
@@ -27,7 +27,7 @@ $wrapper_classes   = apply_filters( 'woocommerce_single_product_image_gallery_cl
 					$full_src          = wp_get_attachment_image_url( $attachment_id, 'large');
 				?>
 				<div class="product_gallery_list_single">
-					<a href="<?php echo $full_src; ?>" data-fancybox="gallery">
+					<a href="<?php echo $full_src; ?>" data-lightbox="product-lightbox" data-title="<?php the_title(); ?>">
 						<img src="<?php echo $thumbnail_src; ?>" alt="">
 					</a>
 				</div>
@@ -36,21 +36,11 @@ $wrapper_classes   = apply_filters( 'woocommerce_single_product_image_gallery_cl
 		</div>
 		<div class="product_gallery_item p-2 md:py-5 md:px-0">
 			<!-- Показываем первую фотку -->
-			<?php foreach ( array_slice($attachment_ids, 0,1) as $attachment_id ): ?>
-				<?php 
-					$gallery_thumbnail = wc_get_image_size( 'gallery_thumbnail' );
-					$thumbnail_size    = 'woocommerce_single';
-					$image_size        = apply_filters( 'woocommerce_gallery_image_size',  $thumbnail_size );
-					$full_size         = apply_filters( 'woocommerce_gallery_full_size', apply_filters( 'woocommerce_product_thumbnails_large_size', 'full' ) );
-					$thumbnail_src     = wp_get_attachment_image_src( $attachment_id, 'woocommerce_single' );
-					$full_src          = wp_get_attachment_image_src( $attachment_id, $full_size );
-				?>
-				<div class="product_gallery_item_main">
-					<a href="<?php echo $full_src[0]; ?>" data-fancybox="gallery">
-						<img src="<?php echo $thumbnail_src[0]; ?>" alt="">
-					</a>
-				</div>
-			<?php endforeach; ?>
+			<div class="product_gallery_item_main">
+				<a href="<?php echo wp_get_attachment_url( $product->get_image_id() ); ?>" data-lightbox="product-lightbox" data-title="<?php the_title(); ?>">
+					<img src="<?php echo wp_get_attachment_url( $product->get_image_id() ); ?>" />
+				</a>
+			</div>
 			<!-- END Показываем первую фотку -->
 		</div>
 	</div>
