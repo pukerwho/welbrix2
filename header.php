dd47fa5e07bc86c47bb164a8680f461f56825b69
Vladimir Samojlov <v-moi@ya.ru>
2021-04-22 13:27:53 +0300

    edit

--- header.php
+++ header.php
@@ -70,15 +70,15 @@
               <div class="header_bottom_icon cart">
                 <a href="<?php echo wc_get_cart_url(); ?>">
                   <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/cart-icon.svg"> 
-                  <span >
-                    <?php 
-                      global $woocommerce;
-                      $count = $woocommerce->cart->cart_contents_count;
-                      if ($count != 0) {
-                        echo $count;  
-                      }
-                    ?>
-                  </span>
+                  <?php 
+                    global $woocommerce;
+                    $count = $woocommerce->cart->cart_contents_count;
+                    if ($count != 0) {
+                      echo '<span>';
+                      echo $count;  
+                      echo '</span>';
+                    }
+                  ?>
                 </a>
               </div>
             </div>
