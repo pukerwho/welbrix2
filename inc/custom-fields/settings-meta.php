85096811cbecf5d4f2092488c7273880d240d60a
Vladimir Samojlov <v-moi@ya.ru>
2021-05-24 16:50:17 +0300

    Edit

--- inc/custom-fields/settings-meta.php
+++ inc/custom-fields/settings-meta.php
@@ -22,10 +22,10 @@ function crb_attach_theme_options() {
       Field::make( 'text', 'crb_main_cat_three_link' . crb_get_i18n_suffix(), 'Ссылка для категории №3' ),
       Field::make( 'complex', 'crb_main_banners' . crb_get_i18n_suffix(), 'Баннеры' )
         ->add_fields( array(
-          Field::make( 'image', 'crb_main_banner_img' . crb_get_i18n_suffix(), 'Картинка' ),
-          Field::make( 'text', 'crb_main_banner_subtitle' . crb_get_i18n_suffix(), 'Подзаголовок' ),
-          Field::make( 'text', 'crb_main_banner_title' . crb_get_i18n_suffix(), 'Заголовок' ),
-          Field::make( 'text', 'crb_main_banner_link' . crb_get_i18n_suffix(), 'Ссылка' ),
+          Field::make( 'image', 'crb_main_banner_img', 'Картинка' ),
+          Field::make( 'text', 'crb_main_banner_subtitle', 'Подзаголовок' ),
+          Field::make( 'text', 'crb_main_banner_title', 'Заголовок' ),
+          Field::make( 'text', 'crb_main_banner_link', 'Ссылка' ),
       ) ),
     ))
     ->add_tab( __('Контакты'), array(
