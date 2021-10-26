2ae0ea918c8b1cf22abdafd84c504260d78ec25b
Puker Who <nawwesele@gmail.com>
2021-10-26 17:43:53 +0300

    edit

--- header.php
+++ header.php
@@ -5,8 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
   <!-- <link rel="stylesheet" href="../css/style.css" inline> -->
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Spartan:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+  
   <?php
     wp_head();
   ?>
